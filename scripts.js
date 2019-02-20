7bac0239d9f901b9179e150740bde3aad78c789a
mohassan <vanmowha@gmail.com>
2019-02-20 22:19:54 +0300

    styled

--- scripts.js
+++ scripts.js
@@ -2,19 +2,23 @@ function tracker() {
   var x = parseInt(document.getElementById("one").value);
   var y = parseInt(document.getElementById("two").value);
   var z = parseInt(document.getElementById("three").value);
-  var triangles = ["Equilateral triangle", "Isosceles triangle", "Scalene triangle"];
-  var response = document.getElementById('response');
+  var triangles = [x, y, z];
+  // var response = document.getElementById("response");
 
 
-  if (x + y > z && z + x = y && z + y = x) {
-    if (x == y && y == z) {
-      response.innerHTML = 'Equilateral triangle'
-    } else if (x == y || y == z || z == x) {
-      response.innerHTML = 'Isosceles triangle'
-    } else {
-      response.innerHTML = 'Scelene triangle'
-    }
+  if (triangles[0] + triangles[1] <= triangles[2] || triangles[0] + triangles[2] <= triangles[1] || triangles[1] + triangles[2] <= triangles[0]) {
+    document.getElementById("response").innerHTML = "A triangle can not be formed from these dimensions ";
+  } else if (triangles[0] === triangles[1] && triangles[1] === triangles[2] && triangles[0] === triangles[2]) {
+    // response.innerHTML = 'Equilateral triangle'
+    document.getElementById("response").innerHTML = "Equilateral triangle.";
+
+  } else if (triangles[0] === triangles[1] || triangles[0] === triangles[2] || triangles[1] === triangles[2]) {
+    // response.innerHTML = 'Isosceles triangle'
+    document.getElementById("response").innerHTML = "Isosceles triangle.";
+  } else if (triangles[0] + triangles[1] > triangles[2] || triangles[1] + triangles[2] > triangles[0] || triangles[0] + triangles[2] > triangles[1]) {
+    // response.innerHTML = 'Scelene triangle'
+    document.getElementById("response").innerHTML = "Scalene triangle.";
   } else {
-    response.innerHTML = 'not a triangle'
+    response.innerHTML = 'not a triangle';
   }
 }
